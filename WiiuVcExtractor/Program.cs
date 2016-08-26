4ee660f41d9b47f36fea69baea8fd640857bdac8
AwakeEchidna <103333418+AwakeEchidna@users.noreply.github.com>
2016-08-26 18:42:11 -0500

    Add project files.

---
+++ WiiuVcExtractor/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+
+namespace WiiuVcExtractor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 2)
+            {
+                Console.WriteLine("Please provide the source rpx file and the destination rom name.");
+                return;
+            }
+
+            string rpxPath = args[0];
+            string destinationPath = args[1];
+
+            if (!File.Exists(rpxPath))
+            {
+                Console.WriteLine("Could not find RPX file at " + rpxPath + ". Please ensure that your filename is correct.");
+                return;
+            }
+
+            // Attempt to decompress the RPX file using wiiurpxtool
+            if (!File.Exists("wiiurpxtool.exe"))
+            {
+                Console.WriteLine("Could not find wiiurpxtool.exe. Please ensure that it is in your working directory.");
+                return;
+            }
+
+            Console.WriteLine("Found wiiurpxtool.exe, decompressing the RPX file...");
+
+            System.Diagnostics.Process extractProcess = System.Diagnostics.Process.Start("wiiurpxtool.exe", "-d " + rpxPath );
+
+            // Wait for extraction to complete...
+            while (extractProcess.HasExited == false)
+            {
+                System.Threading.Thread.Sleep(500);
+            }
+
+            // TODO: Add a check to determine what kind of ROM it is prior to starting up the extractor
+            RomPlatformIdentifier romIdentifier = new RomPlatformIdentifier();
+            RomPlatform platform = romIdentifier.identifyRom(rpxPath);
+
+            switch (platform)
+            {
+                case RomPlatform.NES:
+                    NesVcExtractor nesExtract = new NesVcExtractor();
+                    nesExtract.extractRomFromVcDump(rpxPath, destinationPath);
+                    break;
+                case RomPlatform.SNES:
+                    SnesVcExtractor snesExtract = new SnesVcExtractor();
+                    snesExtract.extractRomFromVcDump(rpxPath, destinationPath);
+                    break;
+            }
+        }
+    }
+}
---
+++ WiiuVcExtractor/RomPlatformIdentifier.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace WiiuVcExtractor
+{
+    public enum RomPlatform { Unknown, NES, SNES };
+
+    public class RomPlatformIdentifier
+    {
+        private const int RPX_HEADER_SIZE = 4;
+        private static readonly byte[] RPX_HEADER_CHECK = { 0x7F, 0x45, 0x4C, 0x46 };
+
+        public RomPlatform identifyRom(string rpxPath)
+        {
+            if (isRpxValid(rpxPath))
+            {
+                if (NesVcExtractor.isValid(rpxPath))
+                {
+                    return RomPlatform.NES;
+                }
+
+                if (SnesVcExtractor.isValid(rpxPath))
+                {
+                    return RomPlatform.SNES;
+                }
+            }
+
+            return RomPlatform.Unknown;
+        }
+
+        private bool isRpxValid(string rpxPath)
+        {
+            using (FileStream fs = new FileStream(rpxPath, FileMode.Open, FileAccess.Read))
+            {
+                using (BinaryReader br = new BinaryReader(fs, new ASCIIEncoding()))
+                {
+                    // advance the binary reader past the offset of the VC file
+                    byte[] header = br.ReadBytes(RPX_HEADER_SIZE);
+
+                    // Validate the header
+                    if (header[0] != RPX_HEADER_CHECK[0] || header[1] != RPX_HEADER_CHECK[1] || header[2] != RPX_HEADER_CHECK[2] || header[3] != RPX_HEADER_CHECK[3])
+                    {
+                        Console.WriteLine("Failed to find valid RPX header at offset 0, not a valid RPX file");
+                        return false;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Found valid RPX Header!");
+                        return true;
+                    }
+                }
+            }
+        }
+    }
+}
