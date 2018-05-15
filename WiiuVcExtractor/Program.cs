b70ac21a86b05e205d9fc1a672bc79fbba74253a
Matthew Newell <matthewtnewell@gmail.com>
2018-05-14 19:08:37 -0500

    Change the SNES extraction extension to .sfc and ensure NES roms are written with the correct size.

--- WiiuVcExtractor/Program.cs
+++ WiiuVcExtractor/Program.cs
@@ -8,7 +8,7 @@ namespace WiiuVcExtractor
 {
     class Program
     {
-        private const string WIIU_VC_EXTRACTOR_VERSION = "0.5.0";
+        private const string WIIU_VC_EXTRACTOR_VERSION = "0.5.1";
 
         static void PrintUsage()
         {
