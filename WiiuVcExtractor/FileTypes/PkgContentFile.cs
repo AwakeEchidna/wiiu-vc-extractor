2b0cd75c3db5b4eff1c50bce032f3dfd7dcc14f0
AwakeEchidna <103333418+AwakeEchidna@users.noreply.github.com>
2019-07-21 16:00:13 -0500

    Initial addition of PC Engine support

---
+++ WiiuVcExtractor/FileTypes/PkgContentFile.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace WiiuVcExtractor.FileTypes
+{
+    // Individual file stored within a .pkg file
+    public class PkgContentFile
+    {
+        string path;
+        byte[] content;
+
+        public string Path { get { return path; } }
+        public byte[] Content { get { return content; } }
+
+        public PkgContentFile(string path, byte[] contentBytes)
+        {
+            this.path = path;
+            this.content = contentBytes;
+        }
+
+        // Writes the content file to a given path or to a relative path if not provided
+        public void Write(string writePath = "")
+        {
+            if (String.IsNullOrEmpty(writePath))
+            {
+                writePath = path;
+            }
+
+            using (BinaryWriter bw = new BinaryWriter(File.Open(writePath, FileMode.Create)))
+            {
+                Console.WriteLine("Writing content file {0} to {1}", path, writePath);
+                bw.Write(content);
+            }
+        }
+    }
+}
---
+++ WiiuVcExtractor/RomExtractors/PceVcExtractor.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Text;
+using System.IO;
+using WiiuVcExtractor.FileTypes;
+using WiiuVcExtractor.Libraries;
+
+namespace WiiuVcExtractor.RomExtractors
+{
+    public class PceVcExtractor : IRomExtractor
+    {
+        private const int PCE_HEADER_LENGTH = 16;
+
+        private PkgFile pkgFile;
+
+        private bool verbose;
+
+        public PceVcExtractor(PkgFile pkgFile, bool verbose = false)
+        {
+            this.verbose = verbose;
+            this.pkgFile = pkgFile;
+        }
+
+        public string ExtractRom()
+        {
+            // Find any PCE files within the pkg file and write them to complete extraction
+            PkgContentFile pceFile = pkgFile.ContentFiles.Find(x => Path.GetExtension(x.Path).ToLower() == ".pce");
+            if (pceFile != null)
+            {
+                pceFile.Write();
+                return pceFile.Path;
+            }
+
+            // If no PCE files exist, attempt to find and process an HCD file and recombine all content files into a usable format
+
+            return "";
+        }
+
+        public bool IsValidRom()
+        {
+            Console.WriteLine("Checking if this is a PC Engine VC title...");
+
+            string entryPointExtension = Path.GetExtension(pkgFile.Header.EntryPoint.ToLower());
+            // Check whether the entry point is a .pce or .hcd file (case-insensitive)
+
+            if (entryPointExtension == ".pce" || entryPointExtension == ".hcd")
+            {
+                Console.WriteLine("PC Engine VC Rom detected! Extension {0} was found in the {1} entry point.", entryPointExtension, pkgFile.Path);
+                return true;
+            }
+
+            Console.WriteLine("Not a PC Engine VC Title");
+            return false;
+        }
+    }
+}
