15b9baadf4054423f0fc4b53fc456bab2126e47a
AwakeEchidna <103333418+AwakeEchidna@users.noreply.github.com>
2019-07-31 19:44:55 -0500

    Version bump

--- WiiuVcExtractor/Program.cs
+++ WiiuVcExtractor/Program.cs
@@ -8,7 +8,7 @@ namespace WiiuVcExtractor
 {
     class Program
     {
-        private const string WIIU_VC_EXTRACTOR_VERSION = "0.7.1";
+        private const string WIIU_VC_EXTRACTOR_VERSION = "0.8.0";
 
         static void PrintUsage()
         {
