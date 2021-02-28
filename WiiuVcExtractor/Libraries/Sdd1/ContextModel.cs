e40e4b5ba2199d7c686dc5205806fd9fdb3b678e
wheatevo <matty@wheatevo.com>
2021-02-28 13:33:25 -0600

    Add SNES S-DD1 extraction support (#51)
    Fixes #50.
    
    Add S-DD1 compressor logic based on Andreas Naive's work with S-DD1 in
    C++.
    
    If S-DD1 data is detected in a given SNES rom, it will be read in from
    the extracted RPX file, recompressed, and injected back into the rom. At
    the moment this should only impact Street Fighter Alpha 2.

---
+++ WiiuVcExtractor/Libraries/Sdd1/ContextModel.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WiiuVcExtractor.Libraries.Sdd1
+{
+    class ContextModel
+    {
+        private byte bitplanesInfo;
+        private byte contextBitsInfo;
+        private List<byte>[] bitplaneBuffer;
+        private byte[] bpBitInd;
+        private byte bitNumber;
+        private int[] byteIndex;
+        private byte currBitplane;
+        private UInt16[] prevBitplaneBits;
+        public ProbabilityEstimationModule pem;
+
+        public ContextModel(ref List<byte>[] bpBuffer)
+        {
+            prevBitplaneBits = new UInt16[8];
+            bpBitInd = new byte[8];
+            byteIndex = new int[8];
+            bitplaneBuffer = bpBuffer;
+        }
+
+        public void PrepareComp(byte header)
+        {
+            bitplanesInfo = (byte)(header & 0x0c);
+            contextBitsInfo = (byte)(header & 0x03);
+            for (int i = 0; i < 8; i++)
+            {
+                byteIndex[i] = 0;
+                bpBitInd[i] = 0;
+                prevBitplaneBits[i] = 0;
+            }
+            bitNumber = 0;
+            switch (bitplanesInfo)
+            {
+                case 0x00:
+                    currBitplane = 1;
+                    break;
+                case 0x04:
+                    currBitplane = 7;
+                    break;
+                case 0x08:
+                    currBitplane = 3;
+                    break;
+            }
+        }
+
+        // returns array with [bit, context]
+        public byte[] GetBit()
+        {
+            byte bit;
+            byte currContext;
+
+            switch (bitplanesInfo)
+            {
+                case 0x00:
+                    currBitplane ^= 0x01;
+                    break;
+                case 0x04:
+                    currBitplane ^= 0x01;
+                    if ((bitNumber & 0x7f) == 0)
+                    {
+                        currBitplane = (byte)((currBitplane + 2) & 0x07);
+                    }
+                    break;
+                case 0x08:
+                    currBitplane ^= 0x01;
+                    if ((bitNumber & 0x7f) == 0)
+                    {
+                        currBitplane ^= 0x02; 
+                    }
+                    break;
+                case 0x0c:
+                    currBitplane = (byte)(bitNumber & 0x07);
+                    break;
+            }
+
+            // Use this where context_bits is used
+            // prevBitplaneBits[currBitplane]
+
+            currContext = (byte)((currBitplane & 0x01) << 4);
+
+            switch (contextBitsInfo)
+            {
+                case 0x00:
+                    currContext |= (byte)(((prevBitplaneBits[currBitplane] & 0x01c0) >> 5) | (prevBitplaneBits[currBitplane] & 0x0001));
+                    break;
+                case 0x01:
+                    currContext |= (byte)(((prevBitplaneBits[currBitplane] & 0x0180) >> 5) | (prevBitplaneBits[currBitplane] & 0x0001));
+                    break;
+                case 0x02:
+                    currContext |= (byte)(((prevBitplaneBits[currBitplane] & 0x00c0) >> 5) | (prevBitplaneBits[currBitplane] & 0x0001));
+                    break;
+                case 0x03:
+                    currContext |= (byte)(((prevBitplaneBits[currBitplane] & 0x0180) >> 5) | (prevBitplaneBits[currBitplane] & 0x0003));
+                    break;
+            }
+
+            if (byteIndex[currBitplane] == bitplaneBuffer[currBitplane].Count)
+            {
+                bit = pem.GetMPS(currContext);
+            }
+            else
+            {
+                bit = 0;
+
+                if (((bitplaneBuffer[currBitplane][byteIndex[currBitplane]]) & (0x80 >> bpBitInd[currBitplane])) != 0)
+                {
+                    bit = 1;
+                }
+
+                if (((++bpBitInd[currBitplane]) & 0x08) != 0)
+                {
+                    bpBitInd[currBitplane] = 0;
+                    byteIndex[currBitplane]++;
+                }
+            }
+
+            prevBitplaneBits[currBitplane] <<= 1;
+            prevBitplaneBits[currBitplane] |= bit;
+
+            bitNumber++;
+
+            byte[] returnArray = new byte[2];
+            returnArray[0] = bit;
+            returnArray[1] = currContext;
+
+            return returnArray;
+        }
+    }
+}
---
+++ WiiuVcExtractor/Libraries/Sdd1/ProbabilityEstimationModule.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WiiuVcExtractor.Libraries.Sdd1
+{
+    class ProbabilityEstimationModule
+    {
+        private ContextModel cm;
+        private GolombCodeEncoder gce;
+        UInt32 inputLength;
+        private struct State
+        {
+            public byte codeNum;
+            public byte nextIfMPS;
+            public byte nextIfLPS;
+
+            public State(byte code, byte mps, byte lps)
+            {
+                codeNum = code;
+                nextIfMPS = mps;
+                nextIfLPS = lps;
+            }
+        }
+
+        private struct SDD1ContextInfo
+        {
+            public byte status;
+            public byte MPS;
+            public SDD1ContextInfo(byte stat, byte mps)
+            {
+                status = stat;
+                MPS = mps;
+            }
+        }
+
+        State[] stateEvolutionTable;
+        SDD1ContextInfo[] contextInfo;
+
+        public ProbabilityEstimationModule(ref ContextModel associatedCM, ref GolombCodeEncoder associatedGCE)
+        {
+            cm = associatedCM;
+            cm.pem = this;
+            gce = associatedGCE;
+            contextInfo = new SDD1ContextInfo[32];
+
+            stateEvolutionTable = new State[]
+            {
+                new State(0,25,25),
+                new State(0, 2, 1),
+                new State(0, 3, 1),
+                new State(0, 4, 2),
+                new State(0, 5, 3),
+                new State(1, 6, 4),
+                new State(1, 7, 5),
+                new State(1, 8, 6),
+                new State(1, 9, 7),
+                new State(2,10, 8),
+                new State(2,11, 9),
+                new State(2,12,10),
+                new State(2,13,11),
+                new State(3,14,12),
+                new State(3,15,13),
+                new State(3,16,14),
+                new State(3,17,15),
+                new State(4,18,16),
+                new State(4,19,17),
+                new State(5,20,18),
+                new State(5,21,19),
+                new State(6,22,20),
+                new State(6,23,21),
+                new State(7,24,22),
+                new State(7,24,23),
+                new State(0,26, 1),
+                new State(1,27, 2),
+                new State(2,28, 4),
+                new State(3,29, 8),
+                new State(4,30,12),
+                new State(5,31,16),
+                new State(6,32,18),
+                new State(7,24,22)
+            };
+        }
+
+        public void PrepareComp(byte header, UInt16 length)
+        {
+            for (byte i = 0; i < 32; i++)
+            {
+                contextInfo[i].status = 0;
+                contextInfo[i].MPS = 0;
+            }
+            inputLength = length;
+            if (((header & 0x0c) != 0x0c) && ((length & 0x0001) != 0))
+            {
+                inputLength++;
+            }
+            inputLength <<= 3;
+        }
+
+        public byte GetMPS(byte context)
+        {
+            return contextInfo[context].MPS;
+        }
+
+        public void Launch()
+        {
+            byte bit;
+            byte context;
+            byte currStatus;
+            byte endOfRun = 0;
+            State currState;
+            byte[] bitReturnBuffer = new byte[2];
+
+            for (UInt32 i = 0; i < inputLength; i++)
+            {
+                bitReturnBuffer = cm.GetBit();
+                bit = bitReturnBuffer[0];
+                context = bitReturnBuffer[1];
+                currStatus = contextInfo[context].status;
+                currState = stateEvolutionTable[currStatus];
+                bit ^= contextInfo[context].MPS;
+                endOfRun = gce.PutBit(currState.codeNum, bit, endOfRun);
+                if (endOfRun != 0)
+                {
+                    if (bit != 0)
+                    {
+                        if ((currStatus & 0xfe) == 0)
+                        {
+                            contextInfo[context].MPS ^= 0x01;
+                        } 
+                        contextInfo[context].status = currState.nextIfLPS;
+                    }
+                    else
+                    {
+                        contextInfo[context].status = currState.nextIfMPS;
+                    }
+                }
+            }
+
+            gce.FinishComp();
+        }
+    }
+}
