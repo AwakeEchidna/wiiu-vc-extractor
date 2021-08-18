AwakeEchidna/wiiu-vc-extractor

commit e40e4b5ba2199d7c686dc5205806fd9fdb3b678e

Add SNES S-DD1 extraction support (#51)
Fixes #50.

Add S-DD1 compressor logic based on Andreas Naive's work with S-DD1 in
C++.

If S-DD1 data is detected in a given SNES rom, it will be read in from
the extracted RPX file, recompressed, and injected back into the rom. At
the moment this should only impact Street Fighter Alpha 2.

WiiuVcExtractor/Program.cs
@@ -8,7 +8,7 @@ namespace WiiuVcExtractor
 {
     class Program
     {
-        private const string WIIU_VC_EXTRACTOR_VERSION = "0.9.0";
+        private const string WIIU_VC_EXTRACTOR_VERSION = "1.0.0";
 
         static void PrintUsage()
         {

WiiuVcExtractor/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("WiiuVcExtractor")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 

WiiuVcExtractor/RomExtractors/SnesVcExtractor.cs
@@ -11,6 +11,8 @@ namespace WiiuVcExtractor.RomExtractors
     {
         private enum SnesHeaderType { NotDetermined, Unknown, HiROM, LoROM };
 
+        private const int VC_HEADER_SIZE = 0x30;
+
         private static readonly byte[] SNES_WUP_HEADER_CHECK = { 0x57, 0x55, 0x50, 0x2D, 0x4A };
         private const int SNES_HEADER_LENGTH = 32;
         private const int SNES_LOROM_HEADER_OFFSET = 0x7FC0;
@@ -44,6 +46,11 @@ namespace WiiuVcExtractor.RomExtractors
         private long romPosition;
         private string vcName;
         private long vcNamePosition;
+        private uint fileSize;
+        private long fileSizePosition;
+        private uint sdd1Offset;
+        private long sdd1OffsetPosition;
+        private uint sdd1DataOffset;
 
         private byte[] snesLoRomHeader;
         private byte[] snesHiRomHeader;
@@ -64,6 +71,10 @@ namespace WiiuVcExtractor.RomExtractors
             headerType = SnesHeaderType.NotDetermined;
             romPosition = 0;
             vcNamePosition = 0;
+            sdd1Offset = 0;
+            sdd1OffsetPosition = 0;
+            fileSize = 0;
+            fileSizePosition = 0;
 
             this.decompressedRomPath = decompressedRomPath;
         }
@@ -130,7 +141,7 @@ namespace WiiuVcExtractor.RomExtractors
 
                 Console.WriteLine("Getting rom data...");
 
-                byte[] pcmData;
+                byte[] appendedData;
 
                 // Browse to the romPosition in the file
                 using (FileStream fs = new FileStream(decompressedRomPath, FileMode.Open, FileAccess.Read))
@@ -145,12 +156,25 @@ namespace WiiuVcExtractor.RomExtractors
                         br.BaseStream.Seek(romPosition, SeekOrigin.Begin);
 
                         snesRomData = br.ReadBytes(romSize);
-                        pcmData = br.ReadBytes((int)(br.BaseStream.Length - romPosition + romSize));
+                        // Read in all data until the end of the file after the rom data based on parsed file size
+                        appendedData = br.ReadBytes((int)(fileSize - (VC_HEADER_SIZE + romSize)));
                     }
                 }
 
-                SnesPcmExtractor pcmExtract = new SnesPcmExtractor(snesRomData, pcmData);
+                SnesPcmExtractor pcmExtract = new SnesPcmExtractor(snesRomData, appendedData);
                 snesRomData = pcmExtract.ExtractPcmData();
+
+                if (sdd1DataOffset > 0)
+                {
+                    Console.WriteLine("Rom uses S-DD1, recompressing S-DD1 data...");
+                    long appendedDataSdd1DataOffset = sdd1DataOffset - (VC_HEADER_SIZE + romSize);
+                    SnesSdd1Extractor sdd1Extract = new SnesSdd1Extractor(snesRomData, appendedData, appendedDataSdd1DataOffset);
+                    snesRomData = sdd1Extract.ExtractSdd1Data();
+                }
+                else
+                {
+                    Console.WriteLine("Rom does not appear to use S-DD1");
+                }
 
                 Console.WriteLine("Writing to " + extractedRomPath + "...");
 
@@ -213,8 +237,26 @@ namespace WiiuVcExtractor.RomExtractors
                                     buffer[15] == 0x00)
                                 {
                                     romPosition = br.BaseStream.Position;
-                                    vcNamePosition = romPosition - 12;
+                                    fileSizePosition = romPosition - 44;   // 0x04 in the header
+                                    sdd1OffsetPosition = romPosition - 24; // 0x18 in the header
+                                    vcNamePosition = romPosition - 12;     // 0x24 in the header
                                     vcName = Encoding.ASCII.GetString(buffer, 4, 8);
+
+                                    // Seek back 44 bytes to gather the fileSize position
+                                    br.BaseStream.Seek(fileSizePosition, SeekOrigin.Begin);
+                                    fileSize = br.ReadUInt32LE();
+
+                                    // Seek back 24 bytes to gather the S-DD1 data offset
+                                    br.BaseStream.Seek(sdd1OffsetPosition, SeekOrigin.Begin);
+                                    sdd1Offset = br.ReadUInt32LE();
+
+                                    // Seek to the S-DD1 data offset and read the S-DD1 header (if any)
+                                    br.BaseStream.Seek(romPosition - VC_HEADER_SIZE + sdd1Offset, SeekOrigin.Begin);
+                                    sdd1DataOffset = br.ReadUInt32LE();
+
+                                    Console.WriteLine("File size is 0x{0:X}", fileSize);
+                                    Console.WriteLine("S-DD1 offset is 0x{0:X}", sdd1Offset);
+                                    Console.WriteLine("S-DD1 data offset is 0x{0:X}", sdd1DataOffset);
 
                                     DetermineHeaderType();
 
@@ -275,10 +317,10 @@ namespace WiiuVcExtractor.RomExtractors
                 {
                     if (verbose)
                     {
-                        Console.WriteLine("Could not determine header type since both HiROM and LoROM headers were valid.");
+                        Console.WriteLine("Could not determine header type since both HiROM and LoROM headers were valid, defaulting to LoROM.");
                     }
-                    // Both appear to be valid, could not determine which is the correct header
-                    headerType = SnesHeaderType.Unknown;
+                    // Both appear to be valid, use LoROM by default
+                    headerType = SnesHeaderType.LoROM;
                 }
                 else
                 {

WiiuVcExtractor/Libraries/Sdd1/BitplanesExtractor.cs
@@ -1,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WiiuVcExtractor.Libraries.Sdd1
+{
+    class BitplanesExtractor
+    {
+        private const byte HEADER_MASK = 0x0C;
+        private byte bitplanesInfo;
+        private UInt16 inputLength;
+        private byte[] inputBuffer;
+        private byte inBitInd;
+        private byte currBitplane;
+        private List<byte>[] bitplaneBuffer;
+        private byte[] bpBitInd;
+        
+        public BitplanesExtractor(ref List<byte>[] bpBuffer)
+        {
+            bpBitInd = new byte[8];
+            bitplaneBuffer = bpBuffer;
+        }
+
+        public void PrepareComp(byte[] inBuffer, byte header)
+        {
+            inputLength = (UInt16)inBuffer.Length;
+            inputBuffer = inBuffer;
+            bitplanesInfo = (byte)(header & HEADER_MASK);
+        }
+
+        public void Launch()
+        {
+            --inputLength;
+            switch (bitplanesInfo)
+            {
+                case 0x00:
+                    currBitplane = 1;
+                    break;
+                case 0x04:
+                    currBitplane = 7;
+                    break;
+                case 0x08:
+                    currBitplane = 3;
+                    break;
+                case 0x0c:
+                    inBitInd = 7;
+                    for (byte i = 0; i < 8; i++)
+                    {
+                        bpBitInd[i] = 0;
+                    }
+                    break;
+            }
+
+            UInt16 counter = 0;
+
+            do
+            {
+                switch (bitplanesInfo)
+                {
+                    case 0x00:
+                        currBitplane ^= 0x01;
+                        bitplaneBuffer[currBitplane].Add(inputBuffer[counter]);
+                        break;
+                    case 0x04:
+                        currBitplane ^= 0x01;
+                        if ((counter & 0x000f) == 0)
+                        {
+                            currBitplane = ((byte)((currBitplane + 2) & 0x07));
+                        }
+                        bitplaneBuffer[currBitplane].Add(inputBuffer[counter]);
+                        break;
+                    case 0x08:
+                        currBitplane ^= 0x01;
+                        if ((counter & 0x000f) == 0)
+                        {
+                            currBitplane ^= 0x02;
+                        }
+                        bitplaneBuffer[currBitplane].Add(inputBuffer[counter]);
+                        break;
+                    case 0x0c:
+                        for (byte i = 0; i < 8; i++) PutBit(i, counter);
+                        break;
+                }
+            } while (counter++ < inputLength);
+        }
+
+        private void PutBit(byte bitplane, UInt16 counter)
+        {
+            List<byte> currBPBuf = bitplaneBuffer[bitplane];
+            byte currBitInd = bpBitInd[bitplane];
+
+            if (currBitInd == 0)
+            {
+                currBPBuf.Add(0);
+            }
+
+            currBPBuf[currBPBuf.Count() - 1] |= (byte)((((inputBuffer[counter]) & (0x80 >> inBitInd)) << inBitInd) >> currBitInd);
+
+            currBitInd++;
+            currBitInd &= 0x07;
+
+            bpBitInd[bitplane] = currBitInd;
+            inBitInd--;
+            inBitInd &= 0x07;
+        }
+    }
+}

WiiuVcExtractor/Libraries/Sdd1/Compressor.cs
@@ -1,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WiiuVcExtractor.Libraries.Sdd1
+{
+    class Compressor
+    {
+        private List<byte>[] bitplaneBuffer;
+        private List<byte> codewordsSequence;
+        private List<byte>[] codewordBuffer;
+        private BitplanesExtractor be;
+        private ContextModel cm;
+        private GolombCodeEncoder gce;
+        private ProbabilityEstimationModule pem;
+        private Interleaver interleaver;
+        public Compressor()
+        {
+            codewordsSequence = new List<byte>();
+            codewordBuffer = new List<byte>[8];
+            bitplaneBuffer = new List<byte>[8];
+            for (int i = 0; i < 8; i++)
+            {
+                bitplaneBuffer[i] = new List<byte>();
+                codewordBuffer[i] = new List<byte>();
+            }
+
+            be = new BitplanesExtractor(ref bitplaneBuffer);
+            cm = new ContextModel(ref bitplaneBuffer);
+            gce = new GolombCodeEncoder(ref codewordsSequence, ref codewordBuffer);
+            pem = new ProbabilityEstimationModule(ref cm, ref gce);
+            interleaver = new Interleaver(ref codewordsSequence, ref codewordBuffer);
+        }
+
+        public byte[] Compress(byte[] inBuf, out UInt32 outLen, byte[] outBuf)
+        {
+            UInt32 minLength;
+            byte[] buffer;
+
+            outBuf = Compress(0, inBuf, out outLen, outBuf);
+
+            minLength = outLen;
+            buffer = new byte[outLen];
+            for (UInt32 i = 0; i < outLen; i++)
+            {
+                buffer[i] = outBuf[i];
+            }
+
+            for (byte j=1; j < 16; j++)
+            {
+                outBuf = Compress(j, inBuf, out outLen, outBuf);
+                if (outLen < minLength)
+                {
+                    minLength = outLen;
+                    for (UInt32 i = 0; i < outLen; i++)
+                    {
+                        buffer[i] = outBuf[i];
+                    }
+                }
+            }
+
+            if (minLength < outLen)
+            {
+                outLen = minLength;
+                for (UInt32 i = 0; i < minLength; i++)
+                {
+                    outBuf[i] = buffer[i];
+                }
+            }
+
+            return outBuf;
+        }
+
+        public byte[] Compress(byte header, byte[] inBuf, out UInt32 outLen, byte[] outBuf)
+        {
+            // Step 1
+            for (byte i = 0; i < 8; i++)
+            {
+                bitplaneBuffer[i].Clear();
+            }
+            be.PrepareComp(inBuf, header);
+            be.Launch();
+
+            // Step 2
+            codewordsSequence.Clear();
+            for (byte i = 0; i < 8; i++)
+            {
+                codewordBuffer[i].Clear();
+            }
+            cm.PrepareComp(header);
+            pem.PrepareComp(header, (UInt16)inBuf.Length);
+            gce.PrepareComp();
+            pem.Launch();
+
+            // Step 3
+            interleaver.PrepareComp(header, outBuf);
+            outBuf = interleaver.Launch(out outLen);
+
+            return outBuf;
+        }
+    }
+}

WiiuVcExtractor/Libraries/Sdd1/ContextModel.cs
@@ -1,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WiiuVcExtractor.Libraries.Sdd1
+{
+    class ContextModel
+    {
+        private byte bitplanesInfo;
+        private byte contextBitsInfo;
+        private List<byte>[] bitplaneBuffer;
+        private byte[] bpBitInd;
+        private byte bitNumber;
+        private int[] byteIndex;
+        private byte currBitplane;
+        private UInt16[] prevBitplaneBits;
+        public ProbabilityEstimationModule pem;
+
+        public ContextModel(ref List<byte>[] bpBuffer)
+        {
+            prevBitplaneBits = new UInt16[8];
+            bpBitInd = new byte[8];
+            byteIndex = new int[8];
+            bitplaneBuffer = bpBuffer;
+        }
+
+        public void PrepareComp(byte header)
+        {
+            bitplanesInfo = (byte)(header & 0x0c);
+            contextBitsInfo = (byte)(header & 0x03);
+            for (int i = 0; i < 8; i++)
+            {
+                byteIndex[i] = 0;
+                bpBitInd[i] = 0;
+                prevBitplaneBits[i] = 0;
+            }
+            bitNumber = 0;
+            switch (bitplanesInfo)
+            {
+                case 0x00:
+                    currBitplane = 1;
+                    break;
+                case 0x04:
+                    currBitplane = 7;
+                    break;
+                case 0x08:
+                    currBitplane = 3;
+                    break;
+            }
+        }
+
+        // returns array with [bit, context]
+        public byte[] GetBit()
+        {
+            byte bit;
+            byte currContext;
+
+            switch (bitplanesInfo)
+            {
+                case 0x00:
+                    currBitplane ^= 0x01;
+                    break;
+                case 0x04:
+                    currBitplane ^= 0x01;
+                    if ((bitNumber & 0x7f) == 0)
+                    {
+                        currBitplane = (byte)((currBitplane + 2) & 0x07);
+                    }
+                    break;
+                case 0x08:
+                    currBitplane ^= 0x01;
+                    if ((bitNumber & 0x7f) == 0)
+                    {
+                        currBitplane ^= 0x02; 
+                    }
+                    break;
+                case 0x0c:
+                    currBitplane = (byte)(bitNumber & 0x07);
+                    break;
+            }
+
+            // Use this where context_bits is used
+            // prevBitplaneBits[currBitplane]
+
+            currContext = (byte)((currBitplane & 0x01) << 4);
+
+            switch (contextBitsInfo)
+            {
+                case 0x00:
+                    currContext |= (byte)(((prevBitplaneBits[currBitplane] & 0x01c0) >> 5) | (prevBitplaneBits[currBitplane] & 0x0001));
+                    break;
+                case 0x01:
+                    currContext |= (byte)(((prevBitplaneBits[currBitplane] & 0x0180) >> 5) | (prevBitplaneBits[currBitplane] & 0x0001));
+                    break;
+                case 0x02:
+                    currContext |= (byte)(((prevBitplaneBits[currBitplane] & 0x00c0) >> 5) | (prevBitplaneBits[currBitplane] & 0x0001));
+                    break;
+                case 0x03:
+                    currContext |= (byte)(((prevBitplaneBits[currBitplane] & 0x0180) >> 5) | (prevBitplaneBits[currBitplane] & 0x0003));
+                    break;
+            }
+
+            if (byteIndex[currBitplane] == bitplaneBuffer[currBitplane].Count)
+            {
+                bit = pem.GetMPS(currContext);
+            }
+            else
+            {
+                bit = 0;
+
+                if (((bitplaneBuffer[currBitplane][byteIndex[currBitplane]]) & (0x80 >> bpBitInd[currBitplane])) != 0)
+                {
+                    bit = 1;
+                }
+
+                if (((++bpBitInd[currBitplane]) & 0x08) != 0)
+                {
+                    bpBitInd[currBitplane] = 0;
+                    byteIndex[currBitplane]++;
+                }
+            }
+
+            prevBitplaneBits[currBitplane] <<= 1;
+            prevBitplaneBits[currBitplane] |= bit;
+
+            bitNumber++;
+
+            byte[] returnArray = new byte[2];
+            returnArray[0] = bit;
+            returnArray[1] = currContext;
+
+            return returnArray;
+        }
+    }
+}

WiiuVcExtractor/Libraries/Sdd1/GolombCodeEncoder.cs
@@ -1,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WiiuVcExtractor.Libraries.Sdd1
+{
+    class GolombCodeEncoder
+    {
+        private List<byte> codewSequence;
+        private List<byte>[] codewBuffer;
+        byte[] bitInd;
+        byte[] MPScount;
+
+        public GolombCodeEncoder(ref List<byte> associatedCWSeq, ref List<byte>[] cwBuf)
+        {
+            codewSequence = associatedCWSeq;
+            codewBuffer = cwBuf;
+            bitInd = new byte[8];
+            MPScount = new byte[8];
+        }
+
+        public void PrepareComp()
+        {
+            for (byte i = 0; i < 8; i++)
+            {
+                MPScount[i] = 0;
+                bitInd[i] = 0;
+            }
+        }
+
+        public byte PutBit(byte codeNum, byte bit, byte endOfRun)
+        {
+            byte rMPScount = MPScount[codeNum];
+
+            if (MPScount[codeNum] == 0)
+            {
+                codewSequence.Add(codeNum);
+            }
+
+            if (bit != 0)
+            {
+                endOfRun = 1;
+                OutputBit(codeNum, 1);
+                for (byte i = 0, aux = 0x01; i < codeNum; i++, aux<<=1)
+                {
+                    byte auxOutputBit = 0;
+                    if ((MPScount[codeNum] & aux) == 0)
+                    {
+                        auxOutputBit = 1;
+                    }
+
+                    OutputBit(codeNum, auxOutputBit);
+                }
+                MPScount[codeNum] = 0;
+            } 
+            else
+            { 
+                if (++(MPScount[codeNum]) == (1<<codeNum))
+                {
+                    endOfRun = 1;
+                    OutputBit(codeNum, 0);
+                    MPScount[codeNum] = 0;
+                }
+                else
+                {
+                    endOfRun = 0;
+                }
+            }
+
+            return endOfRun;
+        }
+
+        public void FinishComp()
+        {
+            for (byte i = 0; i < 8; i++)
+            {
+                if (MPScount[i] != 0)
+                {
+                    OutputBit(i, 0);
+                }
+            }
+        }
+
+        public void OutputBit(byte codeNum, byte bit)
+        {
+            byte rBitInd = bitInd[codeNum];
+            List<byte> rCodewBuffer = codewBuffer[codeNum];
+            byte oBit = 0;
+                
+            if (bit != 0)
+            {
+                oBit = (byte)(0x80 >> bitInd[codeNum]);
+            }
+
+            if (bitInd[codeNum] == 0)
+            {
+                codewBuffer[codeNum].Add(0);
+            }
+
+            codewBuffer[codeNum][codewBuffer[codeNum].Count - 1] |= oBit;
+
+            ++bitInd[codeNum];
+            bitInd[codeNum] &= 0x07;
+        }
+    }
+}

WiiuVcExtractor/Libraries/Sdd1/Interleaver.cs
@@ -1,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WiiuVcExtractor.Libraries.Sdd1
+{
+    class Interleaver
+    {
+        private List<byte> codewSequence;
+        private List<byte>[] codewBuffer;
+        byte[] bitInd;
+        private int[] byteIndex;
+        UInt32 outputLength;
+        byte[] outputBuffer;
+        UInt32 outputBufferIndex;
+        byte oBitInd;
+
+        public Interleaver(ref List<byte> associatedCWSeq, ref List<byte>[] cwBuf)
+        {
+            codewSequence = associatedCWSeq;
+            codewBuffer = cwBuf;
+            bitInd = new byte[8];
+            byteIndex = new int[8];
+        }
+
+        public void PrepareComp(byte header, byte[] outBuf)
+        {
+            outputLength = 0;
+            outputBufferIndex = 0;
+            outputBuffer = outBuf;
+            outputBuffer[outputBufferIndex] = (byte)(header << 4);
+            oBitInd = 4;
+            for (byte i = 0; i < 8; i++)
+            {
+                byteIndex[i] = 0;
+                bitInd[i] = 0;
+            }
+        }
+
+        public byte[] Launch(out UInt32 outLen)
+        {
+            for (int i = 0; i < codewSequence.Count; i++)
+            {
+                if (MoveBit(codewSequence[i]) != 0)
+                {
+                    for (byte j = 0; j < codewSequence[i]; j++)
+                    {
+                        MoveBit(codewSequence[i]);
+                    }
+                }
+            }
+
+            if (oBitInd != 0)
+            {
+                ++outputLength;
+            }
+
+            outLen = outputLength;
+
+            return outputBuffer;
+        }
+
+        private byte MoveBit(byte codeNum)
+        {
+            if (oBitInd == 0)
+            {
+                outputBuffer[outputBufferIndex] = 0;
+            }
+
+            byte bit = (byte)((codewBuffer[codeNum][byteIndex[codeNum]] & (0x80 >> bitInd[codeNum])) << bitInd[codeNum]);
+            outputBuffer[outputBufferIndex] |= (byte)(bit >> oBitInd);
+
+            if ((++bitInd[codeNum] & 0x08) != 0)
+            {
+                bitInd[codeNum] = 0;
+                byteIndex[codeNum]++;
+            }
+
+            if ((++oBitInd & 0x08) != 0)
+            {
+                oBitInd = 0;
+                outputBufferIndex++;
+                ++outputLength;
+            }
+
+            return bit;
+        }
+    }
+}

commit 9b6c9e0511cf579413ba63f45352aa17b0dfed8e

Catch S-DD1 errors
Commit e40e4b5ba2199d7c686dc5205806fd9fdb3b678e caused a regression in
SNES extraction where some small roms cannot be extracted when an S-DD1
header cannot be found.

This change ensures we catch this exception and assume the S-DD1 header
does not exist in those cases.

WiiuVcExtractor/Program.cs
@@ -11,7 +11,7 @@
     /// </summary>
     public class Program
     {
-        private const string WiiUVcExtractorVersion = "2.0.0";
+        private const string WiiUVcExtractorVersion = "2.0.1";
 
         /// <summary>
         /// Prints usage information.

WiiuVcExtractor/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyVersion("2.0.1.0")]
+[assembly: AssemblyFileVersion("2.0.1.0")]

WiiuVcExtractor/RomExtractors/SnesVcExtractor.cs
@@ -267,7 +267,22 @@
 
                             // Seek to the S-DD1 data offset and read the S-DD1 header (if any)
                             br.BaseStream.Seek(this.romPosition - VcHeaderSize + this.sdd1Offset, SeekOrigin.Begin);
-                            this.sdd1DataOffset = br.ReadUInt32LE();
+
+                            try
+                            {
+                                this.sdd1DataOffset = br.ReadUInt32LE();
+                            }
+                            catch (Exception ex)
+                            {
+                                if (this.verbose)
+                                {
+                                    Console.WriteLine(
+                                        "Could not read the S-DD1 data offset, setting the offset to 0 to skip decompression. Error details: {0}",
+                                        ex.Message);
+                                }
+
+                                this.sdd1DataOffset = 0;
+                            }
 
                             Console.WriteLine("File size is 0x{0:X}", this.fileSize);
                             Console.WriteLine("Virtual Console Title offset is 0x{0:X}", this.vcNamePosition);